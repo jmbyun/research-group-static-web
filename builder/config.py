3a86bfe2e13b9b2d1768d063f4ce752ae9b886fc
Jeongmin Byun <jmbyun91@gmail.com>
2020-10-02 15:21:54 +0900

    use data url from config.py file directly

--- builder/config.py
+++ builder/config.py
@@ -2,5 +2,6 @@ import os
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BUILD_PATH = os.path.join(BASE_PATH, 'docs')
-DATA_URL = os.getenv('INPUT_DATA_URL', 'https://docs.google.com/spreadsheets/d/1EDLlUuY2Ia5MKNbCTOftxxSxBaK3C9pRFOIUvMY30eY/edit?usp=sharing')
 API_KEY = os.getenv('INPUT_API_KEY', '')
+
+DATA_URL = 'https://docs.google.com/spreadsheets/d/1EDLlUuY2Ia5MKNbCTOftxxSxBaK3C9pRFOIUvMY30eY/edit?usp=sharing'
