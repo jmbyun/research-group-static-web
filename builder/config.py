c982de814b2318c428a6bc47603926a8a3b0e8a3
Jeongmin Byun <jmbyun91@gmail.com>
2020-10-02 15:28:41 +0900

    fix feeding default api key

--- builder/config.py
+++ builder/config.py
@@ -2,6 +2,6 @@ import os
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BUILD_PATH = os.path.join(BASE_PATH, 'docs')
-API_KEY = os.getenv('INPUT_API_KEY', 'AIzaSyC6dzKyPLK2TsfjiWtxytUE7LQEr64QPwc')
+API_KEY = os.getenv('INPUT_API_KEY', '') or 'AIzaSyC6dzKyPLK2TsfjiWtxytUE7LQEr64QPwc'
 
 DATA_URL = 'https://docs.google.com/spreadsheets/d/1EDLlUuY2Ia5MKNbCTOftxxSxBaK3C9pRFOIUvMY30eY/edit?usp=sharing'
